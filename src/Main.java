a50ea5540875c5057924d435594cc4775e56a8ab
Cristhall Rodríguez <cristhall3004@gmail.com>
2026-04-09 00:26:06 -0600

    Cambios: Se carga el diccionario

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-import ui.EditorUI;
+import dictionary.DictionaryLoader;
+import java.util.HashMap;
+//import ui.EditorUI;
 
-import javax.swing.*;
+//import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
+        /*SwingUtilities.invokeLater(() -> {
             EditorUI ui = new EditorUI();
             ui.setVisible(true);
-        });
+        });*/
+        /*HashMap<String, Boolean> diccionario =
+                DictionaryLoader.cargarDiccionario();*/
+        /*DictionaryLoader diccionario = new DictionaryLoader();
+        dic.cargarDiccionario();*/
+        HashMap<String, Boolean> diccionario =
+                DictionaryLoader.cargarDiccionario("resources/spanish.dic");
+
+        System.out.println("¿Existe casa? " + diccionario.containsKey("casa"));
     }
+    
 }
--- src/dictionary/DictionaryLoader.java
+++ src/dictionary/DictionaryLoader.java
@@ -1,4 +1,34 @@
 package dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class DictionaryLoader {
+    public static HashMap<String, Boolean> cargarDiccionario(String ruta) {
+        HashMap<String, Boolean> diccionario = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            br.readLine(); // omitir primera línea
+
+            String palabra;
+            while ((palabra = br.readLine()) != null) {
+                palabra = palabra.trim().toLowerCase();
+
+                if (!palabra.isEmpty()) {
+                    diccionario.put(palabra, true);
+                }
+            }
+
+            System.out.println("Diccionario cargado correctamente");
+            System.out.println("Total de palabras: " + diccionario.size());
+
+        } catch (IOException e) {
+            System.out.println("Error al cargar el archivo");
+            e.printStackTrace();
+        }
+
+        return diccionario;
+    }
 }
